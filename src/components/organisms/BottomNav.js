845b87664fe6553767e6727986bcef3379e97a74
wingkwong <wingkwong.code@gmail.com>
2020-01-27 16:08:19 +0800

    feat: navigate to corresponding pages

--- src/components/organisms/BottomNav.js
+++ src/components/organisms/BottomNav.js
@@ -7,6 +7,7 @@ import WarningRoundedIcon from '@material-ui/icons/WarningRounded';
 import TimelapseIcon from '@material-ui/icons/Timelapse';
 import InfoRoundedIcon from '@material-ui/icons/InfoRounded';
 import Hidden from '@material-ui/core/Hidden';
+import { UnstyledLink } from '@components/atoms/UnstyledLink';
 
 const mapIcon = name => {
   switch (name) {
@@ -29,6 +30,10 @@ const useStyles = makeStyles({
     width: '100%',
     position: 'fixed',
     bottom: 0,
+  },
+  label: {
+    textTransform: 'uppercase',
+    marginTop: 5,
   }
 });
 
@@ -40,16 +45,20 @@ export default function SimpleBottomNavigation(props) {
 
   return (
     <Hidden smUp implementation="css">
-    <BottomNavigation
-      value={value}
-      onChange={(event, newValue) => {
-        setValue(newValue);
-      }}
-      showLabels
-      className={classes.stickToBottom}
-    >
-      {tabs.map(tab => <BottomNavigationAction label={tab.title} icon={mapIcon(tab.icon)} />)}
-    </BottomNavigation>
-        </Hidden>
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        showLabels
+        className={classes.stickToBottom}
+      >
+        {/* FIXME: the icon is not updated after navigating to other page */}
+        {/* 
+          wingkwong: Cannot use <UnstyledLink> to wrap <BottomNavigationAction> because it has to be a direct child of BottomNavigation
+        */}
+        {tabs.map(tab => <BottomNavigationAction component={UnstyledLink} label={tab.title} icon={mapIcon(tab.icon)} to={tab.to}/>)}
+      </BottomNavigation>
+    </Hidden>
   );
 }
