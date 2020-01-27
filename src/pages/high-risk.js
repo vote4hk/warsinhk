52b606ee4a8c9575464f6b6160f4adced5e8f6a6
Brian Leung <brian_yf_leung@manulifeam.com>
2020-01-27 22:20:49 +0800

    add high-risk page

--- src/pages/high-risk.js
+++ src/pages/high-risk.js
@@ -1,13 +1,73 @@
 import React from "react"
 import SEO from "@/components/templates/SEO"
 import Layout from "@components/templates/Layout"
+import { useTranslation } from "react-i18next"
+import Box from "@material-ui/core/Box"
+import Typography from "@material-ui/core/Typography"
+import { graphql } from "gatsby"
 
-const HighRiskPage = props => (
-  <Layout>
-    <SEO title="HighRiskPage" />
-    <h1>HighRiskPage</h1>
-    <p>TODO</p>
-  </Layout>
-)
+import { BasicCard } from "@components/atoms/Card"
+
+function item(props) {
+  const { node } = props
+  return (
+    <>
+      <Box>
+        <Typography component="span" variant="body2" color="textPrimary">
+          {node.district_zh}
+        </Typography>
+      </Box>
+      <Box>
+        <Typography component="span" variant="h6" color="textPrimary">
+          {node.name_zh}
+        </Typography>
+      </Box>
+      <Box>
+        <Typography component="span" variant="body2" color="textPrimary">
+          {node.last_appear}
+        </Typography>
+      </Box>
+      <Box>
+        <Typography component="span" variant="body2" color="textPrimary">
+          {node.source}
+        </Typography>
+      </Box>
+      <Box>
+        <Typography component="span" variant="body2" color="textPrimary">
+          {node.details}
+        </Typography>
+      </Box>
+    </>
+  )
+}
+
+const HighRiskPage = ({ data, pageContext }) => {
+  const { t } = useTranslation()
+  return (
+    <Layout>
+      <SEO title="HighRiskPage" />
+      <Typography variant="h4">{t("high_risk.title")}</Typography>
+      {data.allHighRisk.edges.map((node, index) => (
+        <BasicCard alignItems="flex-start" key={index} children={item(node)} />
+      ))}
+    </Layout>
+  )
+}
 
 export default HighRiskPage
+
+export const HighRiskQuery = graphql`
+  query {
+    allHighRisk {
+      edges {
+        node {
+          district_zh
+          name_zh
+          sub_district_zh
+          source
+          details
+        }
+      }
+    }
+  }
+`
