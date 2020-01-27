392c35e5be03671e398b39a979048a65351852ca
wingkwong <wingkwong.code@gmail.com>
2020-01-27 16:07:59 +0800

    chore: add empty pages

---
+++ src/pages/high-risk.js
@@ -0,0 +1,18 @@
+import React from "react"
+import SEO from "@/components/templates/SEO"
+import App from "@components/App"
+import Layout from "@components/templates/Layout"
+
+const HighRiskPage = (props) => (
+  <App locale={props.pageContext.locale}>
+    <Layout>
+      <SEO title="HighRiskPage" />
+      <h1>HighRiskPage</h1>
+      <p>
+          TODO
+      </p>
+    </Layout>
+  </App>
+)
+
+export default HighRiskPage
---
+++ src/pages/hygiene-tips.js
@@ -0,0 +1,18 @@
+import React from "react"
+import SEO from "@/components/templates/SEO"
+import App from "@components/App"
+import Layout from "@components/templates/Layout"
+
+const HygieneTipsPage = (props) => (
+  <App locale={props.pageContext.locale}>
+    <Layout>
+      <SEO title="HygieneTipsPage" />
+      <h1>HygieneTipsPage</h1>
+      <p>
+          TODO
+      </p>
+    </Layout>
+  </App>
+)
+
+export default HygieneTipsPage
