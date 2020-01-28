41b49bfb2f7f80372ff1aae566229a387fe342bc
Nandiheath <nandi0315@gmail.com>
2020-01-28 19:14:08 +0800

    🔨fix: manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,13 +52,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `武漢肺炎民間資訊`,
+        short_name: `WARS`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/images/og_share.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
