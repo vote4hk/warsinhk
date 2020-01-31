7af8a56799d10065e473d3782631fc321e339ccc
wingkwong <wingkwong.code@gmail.com>
2020-01-31 14:30:04 +0800

    feat: add cancel button

--- src/components/organisms/AlertMessage.js
+++ src/components/organisms/AlertMessage.js
@@ -1,16 +1,66 @@
 import React from "react"
 import styled from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
+import { useTranslation } from "react-i18next"
+import { mapIcon } from "@components/icons"
+import IconButton from "@material-ui/core/IconButton"
+import Collapse from "@material-ui/core/Collapse"
 import Alert from "@material-ui/lab/Alert"
 import AlertTitle from "@material-ui/lab/AlertTitle"
 import { withLanguage } from "@/utils/i18n"
-import { useTranslation } from "react-i18next"
 
 const AlertMessageContainer = styled.div`
   && {
     margin-bottom: 20px;
   }
 `
+
+const AlertChild = props => {
+  const { edge } = props
+  const { i18n } = useTranslation()
+
+  var {
+    node: { title, message, severity, variant, enabled },
+  } = edge
+
+  const [open, setOpen] = React.useState(true)
+
+  if (enabled === "Y") {
+    title = withLanguage(i18n, edge.node, "title")
+    message = withLanguage(i18n, edge.node, "message")
+
+    severity = severity ? severity : "info"
+    variant = variant ? variant : "standard"
+
+    return (
+      <AlertMessageContainer>
+        <Collapse in={open}>
+          <Alert
+            severity={severity}
+            variant={variant}
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setOpen(false)
+                }}
+              >
+                {mapIcon("close")}
+              </IconButton>
+            }
+          >
+            {title && <AlertTitle>{title}</AlertTitle>}
+            {message}
+          </Alert>
+        </Collapse>
+      </AlertMessageContainer>
+    )
+  }
+
+  return null
+}
 
 const AlertMessage = props => {
   const alert = useStaticQuery(
@@ -37,33 +87,10 @@ const AlertMessage = props => {
     allAlert: { edges },
   } = alert
 
-  const { i18n } = useTranslation()
-
   return (
     <>
       {edges.map((edge, index) => {
-        var {
-          node: { title, message, severity, variant, enabled },
-        } = edge
-
-        if (enabled === "Y") {
-          title = withLanguage(i18n, edge.node, "title")
-          message = withLanguage(i18n, edge.node, "message")
-
-          severity = severity ? severity : "info"
-          variant = variant ? variant : "standard"
-
-          return (
-            <AlertMessageContainer key={index}>
-              <Alert severity={severity} variant={variant}>
-                {title && <AlertTitle>{title}</AlertTitle>}
-                {message}
-              </Alert>
-            </AlertMessageContainer>
-          )
-        }
-
-        return null
+        return <AlertChild edge={edge} index={index} />
       })}
     </>
   )
