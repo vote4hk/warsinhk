ad7a490831b0713da8b1375c7ba8310985378747
Brian Leung <brianleung1017@gmail.com>
2020-02-06 01:24:26 +0800

    🖍feat: calculate age median (#111)

--- src/utils/index.js
+++ src/utils/index.js
@@ -12,3 +12,9 @@ export const loadFromLocalStorage = key => {
   }
   return null
 }
+
+export const median = arr => {
+  const mid = Math.floor(arr.length / 2),
+    nums = [...arr].sort((a, b) => a - b)
+  return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2
+}
