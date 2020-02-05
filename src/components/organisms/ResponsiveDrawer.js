c580849e16b923e34456fa4fc3a4e9d10abc4856
Shing <y.shing@me.com>
2020-02-05 21:47:47 +0800

    Add backgound for current active menu (#103)

--- src/components/organisms/ResponsiveDrawer.js
+++ src/components/organisms/ResponsiveDrawer.js
@@ -93,7 +93,11 @@ function ResponsiveDrawer(props) {
         <Divider />
         <List>
           {pages.map((page, index) => (
-            <UnstyledLink to={getLocalizedPath(i18n, page.to)} key={index}>
+            <UnstyledLink
+              to={getLocalizedPath(i18n, page.to)}
+              key={index}
+              activeClassName={"active"}
+            >
               <ListItem>
                 <ListItemIcon>{mapIcon(page.icon)}</ListItemIcon>
                 <ListItemText primary={t(page.title)} />
