09f03ae1a611fea90f4eaecce11d6c8d97f43a13
Nandiheath <nandi0315@gmail.com>
2020-02-07 01:09:36 +0800

    🔨fix: fix some css issues

--- src/pages/high-risk.js
+++ src/pages/high-risk.js
@@ -17,7 +17,6 @@ import { BasicCard } from "@components/atoms/Card"
 import { withLanguage, getLocalizedPath } from "@/utils/i18n"
 import { Row } from "@components/atoms/Row"
 import Grid from "@material-ui/core/Grid"
-import { components } from "react-select"
 import AsyncSelect from "react-select/async"
 import * as d3 from "d3"
 
@@ -30,7 +29,7 @@ import {
 
 import { saveToLocalStorage, loadFromLocalStorage } from "@/utils"
 
-const colors = d3.scaleOrdinal(d3.schemeAccent).domain([0, 1, 2, 3])
+const colors = d3.scaleOrdinal(d3.schemeAccent).domain([0, 1, 2, 3, 4])
 const KEY_HISTORY_LOCAL_STORAGE = "high-risk-search-history"
 
 const HighRiskCard = styled(Box)``
@@ -60,12 +59,14 @@ const CaseLabel = styled(Box)`
   padding: 4px 6px 4px;
   margin-right: 4px;
   border-radius: 2px;
+  margin-bottom: 4px;
 `
 
 const LabelRow = styled(Row)`
   justify-content: flex-start;
   margin: 0px;
   margin-bottom: 4px;
+  flex-wrap: wrap;
 `
 
 const InfoToolTip = ({ t, title, className, color }) => {
@@ -105,18 +106,20 @@ function item(props, i18n, t) {
           </Typography>
         </Box>
         {node.cases.map((c, index) => (
-          <Grid key={index} container spacing={3}>
+          <Grid key={index} container spacing={1}>
             <Grid item xs={4}>
               <Typography component="span" variant="body2" color="textPrimary">
-                {c.start_date}
+                {c.start_date === c.end_date
+                  ? c.end_date
+                  : `${c.start_date} - ${c.end_date}`}
               </Typography>
             </Grid>
-            <Grid item xs={2}>
+            <Grid item xs={3}>
               <Typography component="span" variant="body2" color="textPrimary">
                 {withLanguage(i18n, c, "action")}
               </Typography>
             </Grid>
-            <Grid item xs={6}>
+            <Grid item xs={5}>
               <LabelRow>
                 {withLanguage(i18n, c, "remarks") && (
                   <InfoToolTip
@@ -125,11 +128,9 @@ function item(props, i18n, t) {
                     color={colors(0)}
                   />
                 )}
-                {c.case_no && (
+                {c.case && (
                   <Link to={getLocalizedPath(i18n, `/cases/#${c.case_no} `)}>
-                    <CaseLabel color={colors(1)}>
-                      {t("high_risk.confirmed_case")}
-                    </CaseLabel>
+                    <CaseLabel color={colors(1)}>{`#${c.case_no}`}</CaseLabel>
                   </Link>
                 )}
                 {c.source_url_1 && (
@@ -139,24 +140,18 @@ function item(props, i18n, t) {
                     </CaseLabel>
                   </MuiLink>
                 )}
+                {c.source_url_2 && (
+                  <MuiLink target="_blank" href={c.source_url_2}>
+                    <CaseLabel color={colors(4)}>
+                      {t("high_risk.source_2")}
+                    </CaseLabel>
+                  </MuiLink>
+                )}
               </LabelRow>
             </Grid>
           </Grid>
         ))}
       </HighRiskCardContent>
-      {/* TODO: The Whole card should be clickable and redirect to map */}
-      {/* <Typography variant="body2">
-        <Link
-          href={`https://maps.google.com/?q=${withLanguage(
-i18n,
-  node,
-  "name"
-          )}`}
-          target="_blank"
-        >
-          {t("text.map")}
-        </Link>
-      </Typography> */}
     </HighRiskCard>
   )
 }
@@ -241,53 +236,42 @@ const HighRiskPage = ({ data, pageContext }) => {
           {mapMode ? t("high_risk.list_mode") : t("high_risk.map_mode")}
         </Button>
       </Row>
-      <AsyncSelect
-        closeMenuOnSelect={false}
-        components={{
-          Option: props =>
-            props.field === "sub_district" ? (
-              <components.Option {...props} />
-            ) : (
-              <components.Option {...props} />
-            ),
-        }}
-        loadOptions={(input, callback) =>
-          callback(filterSearchOptions(allOptions, input, 5))
-        }
-        isMulti
-        placeholder={t("search.placeholder")}
-        defaultOptions={filterSearchOptions(allOptions, null, 10)}
-        // formatGroupLabel={SelectGroupLabel}
-        onChange={selectedArray => {
-          // only append the history
-          if (selectedArray && selectedArray.length > filters.length) {
-            const historiesToSave = [
-              ...histories,
-              selectedArray[selectedArray.length - 1],
-            ].filter((_, i) => i < 10)
-            setHistories(historiesToSave)
-            saveToLocalStorage(
-              KEY_HISTORY_LOCAL_STORAGE,
-              JSON.stringify(historiesToSave)
-            )
-          }
-
-          setFilters(selectedArray || "")
-        }}
-      />
-
       {/* Add Date-time picker for selecting ranges */}
       {mapMode ? (
         <>
           {/* Buy time component.. will get rid of this code once we have a nice map component */}
           <MapContainer
             dangerouslySetInnerHTML={{
-              __html: `< iframe title = "map" src = "https://www.google.com/maps/d/embed?mid=1VdE10fojNRAVr1omckkgKbINL12oj5Bm" width = "100%" height = "100%" ></iframe > `,
+              __html: `<iframe title = "map" src = "https://www.google.com/maps/d/embed?mid=1VdE10fojNRAVr1omckkgKbINL12oj5Bm" width= "100%" height = "100%" ></iframe> `,
             }}
           />
         </>
       ) : (
         <>
+          <AsyncSelect
+            closeMenuOnSelect={false}
+            loadOptions={(input, callback) =>
+              callback(filterSearchOptions(allOptions, input, 5))
+            }
+            isMulti
+            placeholder={t("search.placeholder")}
+            defaultOptions={filterSearchOptions(allOptions, null, 10)}
+            onChange={selectedArray => {
+              // only append the history
+              if (selectedArray && selectedArray.length > filters.length) {
+                const historiesToSave = [
+                  ...histories,
+                  selectedArray[selectedArray.length - 1],
+                ].filter((_, i) => i < 10)
+                setHistories(historiesToSave)
+                saveToLocalStorage(
+                  KEY_HISTORY_LOCAL_STORAGE,
+                  JSON.stringify(historiesToSave)
+                )
+              }
+              setFilters(selectedArray || "")
+            }}
+          />
           {filteredLocations.map((node, index) => (
             <BasicCard
               alignItems="flex-start"
@@ -322,8 +306,8 @@ export const HighRiskQuery = graphql`
           remarks_zh
           source_url_1
           source_url_2
-          start_date(formatString: "YYYY-MM-DD")
-          end_date(formatString: "YYYY-MM-DD")
+          start_date(formatString: "DD/MM")
+          end_date(formatString: "DD/MM")
           type
           case_no
           case {
