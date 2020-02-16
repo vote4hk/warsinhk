698de3601c972cf206459bb5763969be0998c516
OpenHealth HK <openhealthhk@gmail.com>
2020-02-17 03:03:39 +0800

    Reorder and add color to the case summary text

--- src/components/modecules/ConfirmedCasesSummary.js
+++ src/components/modecules/ConfirmedCasesSummary.js
@@ -2,11 +2,13 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { useTranslation } from "react-i18next"
 import { Label } from "../atoms/Text"
+import { Box } from "@material-ui/core"
 import styled from "styled-components"
+import { mapColorForStatus } from "../../utils/colorHelper"
 
-const BoldLabel = styled(Label)`
+const SummaryBox = styled(Box)`
    {
-    font-weight: 700;
+    margin: 10px 0px;
   }
 `
 
@@ -28,18 +30,29 @@ const ConfirmedCasesSummary = props => {
 
   const { t } = useTranslation()
 
+  const statusOrdering = {
+    deceased: 10,
+    critical: 20,
+    serious: 30,
+    hospitalised: 40,
+    discharged: 50,
+  }
+
   return (
-    <React.Fragment>
-      <BoldLabel display="inline">
-        {t("confirmed_case_summary.status_summary")}：
-      </BoldLabel>
-      <Label display="inline">
-        {status
-          .sort((a, b) => b.totalCount - a.totalCount)
-          .map(v => t(`cases.status_${v.fieldValue}`) + "：" + v.totalCount)
-          .join("　")}
-      </Label>
-    </React.Fragment>
+    <SummaryBox>
+      {status
+        .sort(
+          (a, b) => statusOrdering[a.fieldValue] - statusOrdering[b.fieldValue]
+        )
+        .map(v => (
+          <Label
+            display="inline"
+            style={{ color: mapColorForStatus(v.fieldValue).main }}
+          >
+            {t(`cases.status_${v.fieldValue}`)}：{v.totalCount}　
+          </Label>
+        ))}
+    </SummaryBox>
   )
 }
 
---
+++ src/utils/colorHelper.js
@@ -0,0 +1,81 @@
+import {
+  pink,
+  teal,
+  blueGrey,
+  red,
+  green,
+  amber,
+  grey,
+} from "@material-ui/core/colors"
+
+export const mapColorForClassification = classification => {
+  const mapping = {
+    imported: {
+      main: pink[600],
+      contrastText: "#fff",
+    },
+    imported_close_contact: {
+      main: pink[700],
+      contrastText: "#fff",
+    },
+    local: {
+      main: teal[600],
+      contrastText: "#fff",
+    },
+    local_possibly: {
+      main: teal[500],
+      contrastText: "#fff",
+    },
+    local_unknown_source: {
+      main: teal[700],
+      contrastText: "#fff",
+    },
+    local_possibly_close_contact: {
+      main: teal[400],
+      contrastText: "#fff",
+    },
+    local_close_contact: {
+      main: teal[500],
+      contrastText: "#fff",
+    },
+    default: {
+      main: grey[900],
+      contrastText: "#fff",
+    },
+  }
+
+  if (!mapping[classification]) return mapping["default"]
+  return mapping[classification]
+}
+
+export const mapColorForStatus = status => {
+  const mapping = {
+    hospitalised: {
+      main: amber[700],
+      contrastText: "#000",
+    },
+    discharged: {
+      main: green[700],
+      contrastText: "#fff",
+    },
+    serious: {
+      main: red[600],
+      contrastText: "#fff",
+    },
+    critical: {
+      main: red[900],
+      contrastText: "#fff",
+    },
+    deceased: {
+      main: blueGrey[800],
+      contrastText: "#fff",
+    },
+    default: {
+      main: grey[50],
+      contrastText: "#000",
+    },
+  }
+
+  if (!mapping[status]) return mapping["default"]
+  return mapping[status]
+}
