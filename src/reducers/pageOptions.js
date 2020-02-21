0828067cf0ecc0885ee89998aa8b363d07ddeaf1
Koala Yeung <koala@pixelactionstudio.com>
2020-02-21 16:54:22 +0800

    Add a minimum zoom limit
    * Prevent font_zoom to be anything less than 0.5

--- src/reducers/pageOptions.js
+++ src/reducers/pageOptions.js
@@ -4,6 +4,7 @@ export const ALERT_CLOSE = "alert_close"
 export const FONT_ZOOMIN = "font_zoomin"
 export const FONT_ZOOMOUT = "font_zoomout"
 export const FONT_ZOOM_LOCALSTORAGE_KEY = "font_zoom"
+export const FONT_ZOOM_MIN = 0.5
 
 export default (state, action) => {
   switch (action.type) {
@@ -17,8 +18,12 @@ export default (state, action) => {
       return { ...state, fontZoom: state.fontZoom + 0.5 }
     }
     case FONT_ZOOMOUT: {
-      saveToLocalStorage(FONT_ZOOM_LOCALSTORAGE_KEY, state.fontZoom - 0.5)
-      return { ...state, fontZoom: state.fontZoom - 0.5 }
+      const fontZoom = state.fontZoom - 0.5
+      if (fontZoom < FONT_ZOOM_MIN) {
+        return { ...state }
+      }
+      saveToLocalStorage(FONT_ZOOM_LOCALSTORAGE_KEY, fontZoom)
+      return { ...state, fontZoom }
     }
     default:
       return { ...state }
