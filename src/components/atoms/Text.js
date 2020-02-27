7dc4079a5aacc916be1edfe18d085a7405512cf1
Brian Leung <brianleung1017@gmail.com>
2020-02-27 22:45:54 +0800

    🔰fix: missing px

--- src/components/atoms/Text.js
+++ src/components/atoms/Text.js
@@ -3,13 +3,13 @@ import Typography from "@material-ui/core/Typography"
 
 export const Label = styled(Typography)`
   margin-bottom: 3px;
-  font-size: ${props => props.theme.typography.xsmallFontSize};
+  font-size: ${props => props.theme.typography.xsmallFontSize}px;
   color: ${props => props.theme.palette.primary.dark};
 `
 
 export const Paragraph = styled(Typography)`
   margin-bottom: 8px;
-  font-size: ${props => props.theme.typography.smallFontSize};
+  font-size: ${props => props.theme.typography.smallFontSize}px;
 
   && b {
     font-weight: 800;
