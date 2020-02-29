37acfbd9ad143f622ca7d46c053bb0182d822563
Y.Shing <y.shing@me.com>
2020-02-29 16:42:20 +0800

    Fix merge error

--- src/components/highRiskMap.js
+++ src/components/highRiskMap.js
@@ -148,6 +148,7 @@ class HighRiskMap extends Component {
                 closeOnClick: false,
                 closeButton: false,
                 closeOnEscapeKey: false,
+              })
             this.pixiLayer.redraw()
           })
         }
