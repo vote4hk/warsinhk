df3786f725eb6b288ee9203ba1c44159fd7e6dca
Nandiheath <nandi0315@gmail.com>
2020-04-04 00:34:44 +0800

    update according to new design

--- src/components/molecules/dashboard/TrendLineCharts.js
+++ src/components/molecules/dashboard/TrendLineCharts.js
@@ -2,16 +2,62 @@ import React from "react"
 import { useTranslation } from "react-i18next"
 import { useStaticQuery, graphql } from "gatsby"
 import MiniLineChart from "@/components/charts/MiniLineChart"
-import Typography from "@material-ui/core/Typography"
-import Card from "@material-ui/core/Card"
 import styled from "styled-components"
+
+const CHIP_SIZE = 7
+
+const ChartContainer = styled.div`
+  position: relative;
+  height: ${CHIP_SIZE} * 11 + px;
+  background-color: white;
+  border-radius: 15px;
+  box-shadow: 0 0 ${CHIP_SIZE}px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  width: 150px;
+  height: 90px;
+  margin-bottom: 16px;
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: ${CHIP_SIZE * 2}px;
+    height: ${CHIP_SIZE * 6.5}px;
+    top: -${CHIP_SIZE} px;
+    left: -${CHIP_SIZE}px;
+    background-color: ${props => props.color};
+    border-radius: ${CHIP_SIZE}px;
+  }
+
+  span {
+    position: absolute;
+    left: 15px;
+  }
+  .title {
+    top: 6px;
+  }
+  .figure {
+    top: 25px;
+    font-size: 24px;
+    font-weight: 500;
+  }
+  .increase-figure {
+    top: 60px;
+  }
+
+  .mini-chart {
+    position: absolute;
+    left: 40%;
+    width: 60%;
+  }
+`
 
 const Container = styled.div`
   .row {
     display: flex;
     flex-direction: row;
-    .column {
-    }
+    flex-wrap: wrap;
+    justify-content: space-between;
+
     .spacer {
       margin-left: 16px;
     }
@@ -72,99 +118,76 @@ export default props => {
       .reverse()
       .pop()
   }
+  const getIncreasedByField = field => {
+    const array = data.allBotWarsLatestFigures.edges
+      .map(getDataForChart)
+      .map(data => data[field] || 0)
+
+    if (array.length > 1) {
+      return array[0] - array[1]
+    }
+    return 0
+  }
+
+  const chartsData = [
+    {
+      title: t("tredning.accu_confirmed"),
+      field: "confirmed",
+      color: "rgb(266,106,108)",
+    },
+    {
+      title: t("tredning.discharged"),
+      field: "discharged",
+      color: "rgb(72,163,158)",
+    },
+    {
+      title: t("tredning.death"),
+      field: "death",
+      color: "rgb(118,118,118)",
+    },
+    {
+      title: t("tredning.hospitalized"),
+      field: "hospitalised",
+      color: "rgb(242,171,64)",
+    },
+  ]
+
+  const renderChart = ({ title, field, color, index }) => {
+    return (
+      <ChartContainer color={color} key={index}>
+        <span className="title">{title}</span>
+        <span className="figure">{getLastFigureByField(field)}</span>
+        <span className="increase-figure">{getIncreasedByField(field)}</span>
+        <div className="mini-chart">
+          <MiniLineChart
+            data={{
+              showLegend: false,
+              xaxis: data.allBotWarsLatestFigures.edges
+                .map(({ node }) => node.date)
+                .reverse(),
+              fields: [field],
+              max,
+              datasets: [
+                {
+                  line: {
+                    color,
+                  },
+                  data: data.allBotWarsLatestFigures.edges
+                    .map(getDataForChart)
+                    .reverse(),
+                },
+              ],
+            }}
+          />
+        </div>
+      </ChartContainer>
+    )
+  }
 
   return (
     <Container>
       <div className="row">
-        <div className="column">
-          <Typography variant="h6">
-            {t("cases.status_hospitalised")}:
-            {getLastFigureByField("hospitalised")}
-          </Typography>
-          <Card>
-            <MiniLineChart
-              data={{
-                showLegend: false,
-                xaxis: data.allBotWarsLatestFigures.edges
-                  .map(({ node }) => node.date)
-                  .reverse(),
-                fields: ["hospitalised"],
-                max,
-                datasets: [
-                  {
-                    line: {
-                      legend: t("cases.status_hospitalised"),
-                      color: "#ff574f",
-                      // 'stroke-dasharray': '5, 2',
-                    },
-                    data: data.allBotWarsLatestFigures.edges
-                      .map(getDataForChart)
-                      .reverse(),
-                  },
-                ],
-              }}
-            />
-          </Card>
-        </div>
-        <div className="spacer" />
-        <div className="column">
-          <Typography variant="h6">
-            {t("cases.status_discharged")}:{getLastFigureByField("discharged")}
-          </Typography>
-          <Card>
-            <MiniLineChart
-              data={{
-                showLegend: false,
-                xaxis: data.allBotWarsLatestFigures.edges
-                  .map(({ node }) => node.date)
-                  .reverse(),
-                fields: ["discharged"],
-                max,
-                datasets: [
-                  {
-                    line: {
-                      legend: t("cases.discharged"),
-                      color: "#57ff4f",
-                      // 'stroke-dasharray': '5, 2',
-                    },
-                    data: data.allBotWarsLatestFigures.edges
-                      .map(getDataForChart)
-                      .reverse(),
-                  },
-                ],
-              }}
-            />
-          </Card>
-        </div>
-        <div className="spacer" />
-        <div className="column">
-          <Typography variant="h6">
-            {t("dashboard.death")}:{getLastFigureByField("death")}
-          </Typography>
-          <Card>
-            <MiniLineChart
-              data={{
-                showLegend: false,
-                xaxis: data.allBotWarsLatestFigures.edges
-                  .map(({ node }) => node.date)
-                  .reverse(),
-                fields: ["death"],
-                max,
-                datasets: [
-                  {
-                    line: {
-                      color: "#4f334f",
-                      // 'stroke-dasharray': '5, 2',
-                    },
-                    data: data.allBotWarsLatestFigures.edges
-                      .map(getDataForChart)
-                      .reverse(),
-                  },
-                ],
-              }}
-            />
-          </Card>
-        </div>
+        {chartsData.map((c, index) => renderChart({ ...c, index }))}
       </div>
     </Container>
   )
